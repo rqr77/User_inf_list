442c62bf23a09b290fe0018f01178c095a7b2aa9
luckystar <stevezlh@hotmail.com>
2019-06-12 11:41:05 +0800

    Updated

---
+++ src/main/java/com/test/servlet/UpdateServlet.java
@@ -0,0 +1,50 @@
+package com.test.servlet;
+
+
+import com.test.domain.User;
+import com.test.service.impl.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/UpdateServlet")
+public class UpdateServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        int id = Integer.parseInt(request.getParameter("id")) ;
+        String name = request.getParameter("name");
+        String gender = request.getParameter("gender");
+        int age = Integer.parseInt(request.getParameter("age")) ;
+        String address = request.getParameter("address");
+        String qq = request.getParameter("qq");
+        String email = request.getParameter("email");
+
+
+        User updateuser = new User();
+
+        updateuser.setId(id);
+        updateuser.setName(name);
+        updateuser.setGender(gender);
+        updateuser.setAge(age);
+        updateuser.setAddress(address);
+        updateuser.setQq(qq);
+        updateuser.setEmail(email);
+        UserServiceImpl service=new UserServiceImpl();
+        service.update(updateuser);
+        response.sendRedirect(request.getContextPath()+ "/UserListServlet");
+
+
+
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
